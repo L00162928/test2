cbd6b646e1c53ab0d5462d3a55b32544cb5b04f7
Alwyn Benny <L00162928@student.lyit.ie>
2021-03-02 17:16:14 +0000

    Initial files added

---
+++ src/main/java/mavngitdemo/MyApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package mavngitdemo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class MyApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello from LYIT");
+	}
+
+}
